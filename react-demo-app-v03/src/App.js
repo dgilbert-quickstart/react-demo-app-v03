7d25ba4a7915e7a0ef85e0d8220bf7639e5880e1
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-26 19:26:48 +0000

    update react demo app 3 - create-react-app, yarn, vite

--- react-demo-app-v03/src/App.js
+++ react-demo-app-v03/src/App.js
@@ -4,6 +4,7 @@ import {HashRouter, Routes, Route} from "react-router-dom"
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Page1 from "./components/Page1";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>      
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
+            <Route path="/page1" element={<Page1/>}/>
           </Routes>
       </HashRouter>        
     </>
--- react-demo-app-v03/src/components/NavBar.js
+++ react-demo-app-v03/src/components/NavBar.js
@@ -6,7 +6,8 @@ function NavBar()
         <>
             <nav>
                 <Link to="/">Home</Link> {" | "}
-                <Link to="/about">About</Link> 
+                <Link to="/about">About</Link> {" | "}
+                <Link to="/page1">Page1</Link> 
             </nav>
         </>
     )
