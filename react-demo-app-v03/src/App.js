76dadab927069a2f62c6c240407ec70af7ddaadf
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-26 15:58:50 +0000

    update react demo app 3 - create-react-app, yarn, vite

--- react-demo-app-v03/src/App.js
+++ react-demo-app-v03/src/App.js
@@ -1,16 +1,10 @@
+import Home from "./components/Home";
+
 function App() {
   return (
     <>
-            <p>Demo react app v3.0</p>
-            <div>
-              <a href="#">useRef demo 1</a><p></p>
-              <a href="#">useState demo 1</a><p></p>              
-            </div>
-            <div>
-              [displa info 1..]
-            </div>
-            <p>Footer @ 2025</p>
-      </>
+      <Home/>       
+    </>
   );
 }
 
