1abf56837be86be1237d132395514425c3e955c7
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-26 16:36:40 +0000

    update react demo app 3 - create-react-app, yarn, vite

--- react-demo-app-v03/src/components/Home.js
+++ react-demo-app-v03/src/components/Home.js
@@ -1,10 +1,23 @@
 function Home() {
+
+    function lnkdemo1(){
+      const msgText = "#App::lnkdemo1"
+
+      console.log(msgText)
+    }
+    
+    const lnkdemo2 = () => {
+      const msgText = "#App::lnkdemo2"
+
+      console.log(msgText)
+    }
+
     return (
       <>
               <p>Demo react app v3.0</p>
               <div>
-                <a href="#">useRef demo 1</a><p></p>
-                <a href="#">useState demo 1</a><p></p>              
+                <a href="#" onClick={lnkdemo1}>useRef demo 1</a><p></p>
+                <a href="#" onClick={lnkdemo2}>useState demo 1</a><p></p>              
               </div>
               <div>
                 [displa info 1..]
