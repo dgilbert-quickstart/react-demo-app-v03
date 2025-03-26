852c76ada437dda953f555588a31efee506cf186
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-26 18:49:12 +0000

    update react demo app 3 - create-react-app, yarn, vite

--- react-demo-app-v03/src/App.js
+++ react-demo-app-v03/src/App.js
@@ -1,13 +1,15 @@
 //import {BrowserRouter, Routes, Route} from "react-router-dom"
 //-- hashrouter - used with GitHub pages 
 import {HashRouter, Routes, Route} from "react-router-dom"
+import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 
 function App() {
   return (
     <>
-        <HashRouter>            
+        <HashRouter>
+          <NavBar/>
           <Routes>      
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
